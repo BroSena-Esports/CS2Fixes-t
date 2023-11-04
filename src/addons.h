5308e021f379e938d5cfc24b9d28a7b3f90f89ac
Vlad-Szilard Paiu <64710238+paiuvlad@users.noreply.github.com>
2023-11-04 13:21:35 +0200

    Update addons.h

--- src/addons.h
+++ src/addons.h
@@ -1,34 +1,3 @@
-/* ************************************Admins chat**************************************************************
-*/ 
-CON_COMMAND_CHAT(u, "admins chat")
-{
-    if (!player)
-        return;
-
-    int iCommandPlayer = player->GetPlayerSlot(); 
-
-    ZEPlayer *pPlayer = g_playerManager->GetPlayer(iCommandPlayer);
-    if (args.ArgC() < 2)
-    {
-        
-        ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIX "Usage: /u <message> to admins");
-        return;
-    }
-	
-for (int i = 0; i < MAXPLAYERS; i++)
-	{
-    ZEPlayer* pAdmin = g_playerManager->GetPlayer(i);
-    CBasePlayerController* cPlayer = (CBasePlayerController*)g_pEntitySystem->GetBaseEntity((CEntityIndex)(i + 1));
-
-    if (!cPlayer || !pAdmin || pAdmin->IsFakeClient() || !pAdmin->IsAdminFlagSet(ADMFLAG_SLAY))
-        continue;
-        ClientPrint(cPlayer, HUD_PRINTTALK," \3*************\14Admins Chat\3*************");
-        ClientPrint(cPlayer, HUD_PRINTTALK, " \7[Admins]\4 %s \1from \7%s ", args.ArgS(), player->GetPlayerName());
-        ClientPrint(cPlayer, HUD_PRINTTALK, " \3**************************************");
-	}
-	//ClientPrint(cPlayer, HUD_PRINTTALK, " \7[To Admins] \4%s \1, message sent to \7Admins", args.ArgS());
-	ClientPrint(player, HUD_PRINTTALK, " \7[To Admins] Message: \4%s \1, sent to \7Admins", args.ArgS());
-}
 //***************************************************Reset Score***********************************************
 CON_COMMAND_CHAT(rs, "reset your score")
 {
@@ -125,7 +94,18 @@ CON_COMMAND_CHAT(medic, "medic")
 		return;
 	}
 
-	health = pEnt->m_iHealth() + 50;
+	if (pZEPlayer->IsAdminFlagSet(ADMFLAG_ROOT))
+	{
+		health = pEnt->m_iHealth() + 50;
+	}
+	else if (pZEPlayer->IsAdminFlagSet(ADMFLAG_SLAY))
+	{
+		health = pEnt->m_iHealth() + 30;
+	}
+	else
+	{
+		health = pEnt->m_iHealth() + 20;
+	}
 
 	if (health > 100)
 		health = 100;
